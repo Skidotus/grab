0f9c1c3c411a565034e66f1a36abde2e7644c60c
Lutfil Hadi <lutfilhadi1999@gmail.com>
2025-10-13 11:22:36 +0800

    styles css updated link

--- login.php
+++ login.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     
-    <link rel="stylesheet" href="/styles/login.css">
+    <link rel="stylesheet" href="styles/login.css">
 </head>
 <body bgcolor="Black">
     <div class="login">
