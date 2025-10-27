daa1ec071d0282a928c2325cb9808ea665ecef34
Lutfil Hadi <lutfilhadi1999@gmail.com>
2025-10-27 11:54:32 +0800

    cleaned  up form

--- booking.php
+++ booking.php
@@ -23,34 +23,50 @@ if (!isset($_SESSION["email"])) {
 
 </head>
 
-<body class="bg-gray-600">
-    <div class="bg-white w-auto h-auto  mx-8 my-5  p-2 shadow-lg rounded-2xl border-gray-50 flex flex-col gap-2">
+<body class="bg-gray-900">
+    <div
+        class="bg-gray-800 text-white w-auto h-auto  mx-8 my-5  p-2 shadow-lg rounded-2xl border-gray-50 flex flex-col gap-2">
         <div>
-            <h1 class="bg-gray-100 text-center text-2xl rounded-lg">Booking</h1>
+            <h1 class=" text-center text-2xl rounded-lg">Booking</h1>
         </div>
-
-
-        <div id="map" class="m-1 p-1 w-auto h-100 border-2 rounded-2xl"></div>
-
-        <button onclick="getLocation()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Click
-            for your current location</button>
-        <p class="bg-amber-50 flex w-40 h-45" id="coords"></p>
-
         <div>
-            <form>
-                <label for="fname">First name:</label><br>
-                <input type="text" id="fname" name="fname"><br>
-                <label for="lname">Last name:</label><br>
-                <input type="text" id="lname" name="lname">
+            <button onclick="getLocation()"
+                    class="mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Click to pickup at current destination
+            </button>
+
+            <form class="flex flex-wrap flex-col" method="POST">
+                <label class="flex-1 p-2 mx-2 text-lg " for="pickupDestination">Pickup Location</label><br>
+                <input id="pickupInput" class="flex-4 bg-gray-700 p-2 mx-2  rounded-lg " type="text" id="pickupDestination"
+                    name="pickupDestination"><br>
+                <label class="flex-1 p-2 mx-2 " for="lname">Dropoff Location</label>
+                <input  class="flex-4 bg-gray-700 p-2 mx-2 mb-4 rounded-lg" type="text" id="lname" name="lname">
+                
+                <!-- <p class=" flex w-40 h-45" id="coords"></p> -->
+                <div id="map" class="m-2 p-1 w-auto h-96 border-2 rounded-lg"></div>
+                <div class="text-white flex justify-end ">
+                    <button class=" mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+
+                </div>
+
+                
             </form>
 
         </div>
+
+
+
+
+
+
     </div>
+
 
 
     <!-- script below -->
     <script>
         const tempat = document.getElementById("coords");
+        //below for form input fill
+        const borangPickup = document.getElementById("pickupInput")
 
         function getLocation() {
             if (navigator.geolocation) {
@@ -62,7 +78,17 @@ if (!isset($_SESSION["email"])) {
         }
 
         function success(position) {
-            tempat.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+            let current_coords = [position.coords.longitude, position.coords.latitude];
+            new maplibregl.Marker({ color: '#4927F5' }) // Custom color
+                // Set position using the markerCoords variable
+                .setLngLat(current_coords)
+                // Add a simple popup
+                .setPopup(new maplibregl.Popup({ offset: 25 })
+                    .setHTML('<h3>Current</h3>'))
+                // Add the marker to the map
+                .addTo(map);
+            borangPickup.value = "Latitude:"+position.coords.latitude+" Longitude:"+position.coords.longitude;
+            //tempat.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 
         }
         function error() {
@@ -77,7 +103,42 @@ if (!isset($_SESSION["email"])) {
             //101.6841 ,101.6841 ,3.1319
             zoom: 16,
             container: 'map',
-        })
+        });
+
+        const markerCoords = [101.79947305762809, 2.935063798197489]
+
+
+
+        map.on('load', () => {
+
+
+
+
+            // Create the marker instance
+            new maplibregl.Marker({ color: '#FF0000' }) // Custom color
+                // Set position using the markerCoords variable
+                .setLngLat(markerCoords)
+                // Add a simple popup
+                .setPopup(new maplibregl.Popup({ offset: 25 })
+                    .setHTML('<h3>A2</h3>'))
+                // Add the marker to the map
+                .addTo(map);
+
+            // Add navigation control
+
+        });
+
+        map.on('load', () => {
+            map.addControl(new maplibregl.NavigationControl({
+                visualizePitch: true,
+                visualizeRoll: true,
+                showZoom: true,
+                showCompass: true
+            }));
+
+        });
+
+
     </script>
 
 </body>
