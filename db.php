6529d0bc029d0f62294004fb888612dee241e79d
Haikal <horob184@gmail.com>
2025-10-29 23:18:43 +0800

    update and added new function

--- db.php
+++ db.php
@@ -1,23 +1,36 @@
 <?php
     $servername = '127.0.0.1';
     $dbname = 'student_transport';
-    $dbusername = 'pma';
-    $dbpassword = 'SuperStrongPass123!';
-    // $dbusername = 'root';
-    // $dbpassword = '';
+    // $dbusername = 'pma';
+    // $dbpassword = 'SuperStrongPass123!';
+    $dbusername = 'root';
+    $dbpassword = '';
 
     $connection = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 
-    function register($username, $email, $password) {
+    function register($username, $email, $password, $phonenumber, $address, $birthdate) {
         global $connection;
-        $sql = "INSERT INTO user_creds (User_Name, User_Email, User_Pass) VALUES ('$username', '$email', '$password')";
+        $sql = "INSERT INTO user_creds (User_Name, User_Email, User_Pass, User_Phone, User_Address, User_Birthdate) VALUES ('$username', '$email', '$password', '$phonenumber', '$address', '$birthdate')";
+        return $connection->query($sql);
+    }
+
+    function registerdriver($drusername, $dremail, $drpassword, $drphonenumber, $draddress, $drbirthdate) {
+        global $connection;
+        $sql = "INSERT INTO drivers (Driver_Name, Driver_Email, Driver_Pass, Driver_Phone, Driver_Address, Driver_Birthdate) VALUES ('$drusername', '$dremail', '$drpassword', '$drphonenumber', '$draddress', '$drbirthdate')";
         return $connection->query($sql);
     }
 
     function login($email, $password) {
         global $connection;
         $sql = "SELECT * FROM user_creds WHERE User_Email = '$email' AND User_Pass = '$password'";
+        $result = $connection->query($sql);
+        return $result->fetch_assoc();
+    }
+
+    function logindriver($dremail, $drpassword) {
+        global $connection;
+        $sql = "SELECT * FROM drivers WHERE Driver_Email = '$dremail' AND Driver_Pass = '$drpassword'";
         $result = $connection->query($sql);
         return $result->fetch_assoc();
     }
@@ -42,9 +55,15 @@
         return $result->fetch_assoc();
     }
 
-    function updateByID($id, $username, $email, $password){
+    function updateByID($id, $username, $email, $password, $address, $birthdate, $phonenumber){
         global $connection;
-        $sql = "UPDATE user_creds SET User_Name = '$username', User_Email = '$email', User_Pass = '$password' WHERE User_ID = '$id'";
+        $sql = "UPDATE user_creds SET User_Name = '$username', User_Email = '$email', User_Pass = '$password', User_Address = '$address', User_Birthdate = '$birthdate', User_Phone = '$phonenumber' WHERE User_ID = '$id'";
+        return $connection->query($sql);
+    }
+
+     function DriverupdateByID($drid, $drusername, $dremail, $drpassword, $draddress, $drbirthdate, $drphonenumber){
+        global $connection;
+        $sql = "UPDATE drivers SET Driver_Name = '$drusername', Driver_Email = '$dremail', Driver_Pass = '$drpassword', Driver_Address = '$draddress', Driver_Birthdate = '$drbirthdate', Driver_Phone = '$drphonenumber' WHERE Driver_ID = '$drid'";
         return $connection->query($sql);
     }
 
@@ -54,4 +73,14 @@
         $result = $connection->query($sql);
         return $result->fetch_assoc();
     }
+
+    // Select a driver row by email from the drivers table
+    function selectDriverByEmail($email){
+        global $connection;
+        $sql = "SELECT * FROM drivers WHERE Driver_Email = '$email'";
+        $result = $connection->query($sql);
+        return $result->fetch_assoc();
+    }
+
+    
 ?>
